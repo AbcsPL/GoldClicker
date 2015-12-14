562ac62c2ca2c9ed7f81f3ac33da0fb91df9643c
dawidokox1 <jestemdawid213@hotmail.com>
2015-12-14 18:00:39 +0100

    Update costam.js

--- costam.js
+++ costam.js
@@ -1,5 +1,16 @@
-//alpha 0.0.1
-var clicks = 0
-var click = function(){
-  clicks++
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
 }
