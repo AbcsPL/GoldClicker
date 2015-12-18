5cb063dca4a9baa1ec377a2410a2cba6b0ac61c8
dawidokox1 <jestemdawid213@hotmail.com>
2015-12-18 14:03:02 +0100

    Update clickEvent.js

--- JS/clickEvent.js
+++ JS/clickEvent.js
@@ -1,6 +1,24 @@
+var SPC = 0;
+var SPS = 0;
+//Points changer
 function points()
 {
-    document.getElementById(click).innerHtml;
-}
-var scoreCounter = document.getElementById(click).valve
+    document.getElementById(click).innerHtml +SPC;
+};
+var scoreCounter = document.getElementById(click).valve;
 points(scoreCounter);
+//ScorePerSec
+setTimeout(function(){
+    if(SPC > 0){
+        points(SPC);
+    }
+},1000);
+//Bonuses
+if(upgrA == 1){
+    SPC + 1;
+    upgr = 0;
+};
+if(upgrB == 1){
+    SPS + 0,1;
+    upgrB = 0;
+};
